ca3cc3d7a1f5c19bb9c63971ea3e892fba51d30a
joaotemponi <150601366+joaotemponi@users.noreply.github.com>
2024-05-14 09:27:12 -0300

    Update atv2.ts

--- atv2.ts
+++ atv2.ts
@@ -1,7 +1,5 @@
 let numerosPares: number[] = new Array(31);
 
-for (let n1: number = 0, n2: number = 0; n1 <= 60; n1 += 2, n2++) {
-    numerosPares[n2] = n1;
+for (let i = 0; i <= 60; i += 2) {
+    console.log(i);
 }
-
-console.log(`Números pares de até 60 são: ${numerosPares}`);
