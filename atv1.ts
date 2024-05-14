31d4a66112565301b225caeab5e96f6e9df85e05
joaotemponi <150601366+joaotemponi@users.noreply.github.com>
2024-05-14 09:06:42 -0300

    Update atv1.ts

--- atv1.ts
+++ atv1.ts
@@ -1,5 +1,5 @@
 let soma: number = 0;
-for (let n: number = 0; n <= 50; n++) {
+for (let n = 0; n <= 50; n++) {
     soma += n;
-    console.log(`A soma de todos números do 0 ao 50 é ${soma}`);
 }
+console.log(`A soma de todos números do 0 ao 50 é ${soma}`);
